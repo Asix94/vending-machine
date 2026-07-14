12d4f59db5b81dae71e38574b7044d93fbe4f957
Carlos Ramos <carlosrt1994@gmail.com>
2026-07-14 15:18:29 +0200

    feat(wallet): add create wallet POST endpoint

---
+++ src/Controller/CreateWalletController.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Wallet\Application\CreateWalletUseCase;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+use const JSON_PRESERVE_ZERO_FRACTION;
+
+final readonly class CreateWalletController
+{
+    public function __construct(private CreateWalletUseCase $createWalletUseCase)
+    {
+    }
+
+    #[Route('/wallets', name: 'wallet_create', methods: ['POST'])]
+    public function __invoke(): JsonResponse
+    {
+        $response = ($this->createWalletUseCase)();
+
+        $jsonResponse = new JsonResponse(
+            null,
+            Response::HTTP_CREATED,
+            [
+                'Location' => sprintf('/wallets/%s', $response->walletId),
+            ],
+        );
+
+        $jsonResponse->setEncodingOptions($jsonResponse->getEncodingOptions() | JSON_PRESERVE_ZERO_FRACTION);
+        $jsonResponse->setData($response->toArray());
+
+        return $jsonResponse;
+    }
+}
